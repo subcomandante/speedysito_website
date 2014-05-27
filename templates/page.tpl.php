236783c08167550aa79cc0ad8f7bb58f091a1b2a
root <root@alap.(none)>
2014-05-27 18:56:56 +0200

    Backport of clearfix

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -11,13 +11,13 @@
 <div id="page">
 
   <div id="header_top">
-  <div class="ss_row_wrapper">
+  <div class="ss_row_wrapper clearfix">
     <?php print render($page['header_top']); ?>
   </div>
   </div>
 
   <header class="header" id="header" role="banner">
-  <div class="ss_row_wrapper">
+  <div class="ss_row_wrapper clearfix">
     <?php if ($logo || $site_name): ?>
       <div class="header__logo-and-name" id="logo-and-name">
         <?php if ($logo): ?>
@@ -56,14 +56,14 @@
   </header>
 
   <div id="preface">
-  <div class="ss_row_wrapper">
+  <div class="ss_row_wrapper clearfix">
     <?php print render($page['preface']); ?>
   </div>
   </div>
 
 
   <div id="main">
-  <div class="ss_row_wrapper">
+  <div class="ss_row_wrapper clearfix">
 
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
