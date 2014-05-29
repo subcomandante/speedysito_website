3a87dc5c4d1bb1c3d9cf112ba02a0940d5ecb0a3
Michele Castigliego <michele.castigliego@altasis.it>
2014-05-29 15:11:44 +0200

    Backporting regions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -121,9 +121,9 @@
   </div>
   </div>
 
-  <div id="region_services">
-  <div class="region_services_inner ss_row_wrapper clearfix">
-    <?php print render($page['region_services']); ?>
+  <div id="footer_top">
+  <div class="region_footer_top_inner ss_row_wrapper clearfix">
+    <?php print render($page['footer_top']); ?>
   </div>
   </div>
 
